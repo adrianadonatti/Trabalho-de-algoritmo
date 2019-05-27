eca33bc33d1308d186f7947544d6f0c76400ed73
Adriana Donatti <51063010+adrianadonatti@users.noreply.github.com>
2019-05-26 22:24:46 -0300

    Update ex8.java

--- ex8.java
+++ ex8.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Ex08 {
-	
+
     /**
      * @param args the command line arguments
      */
@@ -11,7 +11,15 @@ public class Ex08 {
     	    int n = 6; 
     	    double v[][] = new double[n][n]; 
     	    int i,i2; 
-
+    	    String[] nome = new String[6];
+    	    
+    	    for (i=1; i<6; i++) {
+    	    	 System.out.println("Digite o nome do aluno n"+(i)+"º : ");
+    	    	 nome[i] = sc.nextLine();
+    	    	
+    	    }
+    	    
+    	    
     	    for (i=1; i<n; i++) {
         	    for (i2=1; i2<3; i2++) {
         	      System.out.printf("Informe a Nota "+i2 + " do Aluno " + i +" : ");
@@ -22,7 +30,7 @@ public class Ex08 {
     	    
     	    for (i=1; i<n; i++) {
      	    	 System.out.printf("\n");
-    	    	 System.out.printf("O aluno "+i +" teve a nota  1 : "+ v[i][1] + "");
+    	    	 System.out.printf("O aluno "+ nome[i] +" teve a nota  1 : "+ v[i][1] + "");
     	    	 System.out.printf(", E a nota  2 : "+ v[i][2] + "");
     	    	 System.out.printf("- A média do aluno : "+ i +"  foi de : "+ (v[i][1]+v[i][2])/2 + "");
     	    	 
