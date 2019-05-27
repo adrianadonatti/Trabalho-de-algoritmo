066f40ecc06d6eca8927d61e77908b3b76935d6b
Adriana Donatti <51063010+adrianadonatti@users.noreply.github.com>
2019-05-26 22:49:25 -0300

    Update ex8.java

--- ex8.java
+++ ex8.java
@@ -6,37 +6,35 @@ public class Ex08 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    		Scanner sc = new Scanner(System.in);
-    	 
-    	    int n = 6; 
-    	    double v[][] = new double[n][n]; 
-    	    int i,i2; 
-    	    String[] nome = new String[6];
-    	    
-    	    for (i=1; i<6; i++) {
-    	    	 System.out.println("Digite o nome do aluno n"+(i)+"º : ");
-    	    	 nome[i] = sc.nextLine();
-    	    	
-    	    }
-    	    
-    	    
-    	    for (i=1; i<n; i++) {
-        	    for (i2=1; i2<3; i2++) {
+    	   Scanner sc = new Scanner(System.in);
+   	 
+	    int n = 6; 
+	    double nota[][] = new double[n][n]; 
+	    int i,i2; 
+	    String[] nome = new String[6];
+	    
+	    
+	    for (i=1; i<n; i++) {
+	    	 System.out.println("Digite o nome do aluno n"+(i)+"º : ");
+	    	 nome[i] = sc.next();
+	    	
+		    for (i2=1; i2<3; i2++) {
 		      System.out.printf("Informe a Nota "+i2 + " do Aluno " + nome[i] +" : ");
-	    	      v[i][i2] = sc.nextDouble();
-        	    }
-    	    }
-    	    
-    	    
-    	    for (i=1; i<n; i++) {
-     	    	 System.out.printf("\n");
-    	    	 System.out.printf("O aluno "+ nome[i] +" teve a nota  1 : "+ v[i][1] + "");
-    	    	 System.out.printf(", E a nota  2 : "+ v[i][2] + "");
-    	    	 System.out.printf("- A média do aluno : "+ i +"  foi de : "+ (v[i][1]+v[i][2])/2 + "");
-    	    	 
-    	    	 System.out.printf("\n");
+		      nota[i][i2] = sc.nextDouble();
+		    }
+	    }
+	    
+	    
+	    for (i=1; i<n; i++) {
+ 	    	 System.out.printf("\n");
+	    	 System.out.printf("O aluno "+ nome[i] +" teve a nota  1 : "+ nota[i][1] + "");
+	    	 System.out.printf(", E a nota  2 : "+ nota[i][2] + "");
+	    	 System.out.printf("- A média do aluno : "+ i +"  foi de : "+ (nota[i][1]+nota[i][2])/2 + "");
+	    	 
+	    	 System.out.printf("\n");
 
-    	    }
+	    }
+
 
     }
 }
